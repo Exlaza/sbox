fbeb57a88f1f377fead4cd9bade8b8784a14ca0b
Ekansh Jain <ekansh.jain15@gmail.com>
2019-12-10 23:38:11 +0530

    fix: Deleted main.go and added a new main.gowith proper directory structure

---
+++ cmd/web/handlers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Okay, this is my homepage"))
+
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Well here you go, snippet of id %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not allowed", 405)
+	}
+	w.Write([]byte("Here we create a new snippet"))
+
+}
